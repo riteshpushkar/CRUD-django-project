8e8955ae5d49bb4349d997d4e3b26d6769f5a8ee
Ritesh Pushkar <riteshpushkar01@gmail.com>
2022-06-24 18:03:00 +0700

    CRUD PROJECT

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+# from unicodedata import name
+from django.urls import URLPattern, path
+from .views import Home, Add_Student, Delete_Student, EditStudent
+urlpatterns = [
+
+        path('',Home.as_view(), name = 'home' ),
+        path('add-student/', Add_Student.as_view(), name = 'add-student'),
+        path('delete-student/', Delete_Student.as_view(), name='delete-student'),
+        path('edit-student/<int:id>/', EditStudent.as_view(), name='edit-student')
+        
+
+]
